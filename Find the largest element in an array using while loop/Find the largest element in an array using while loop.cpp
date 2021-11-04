3f79480b84e25c3e3e97d3cb21964ce28483a639
ashikurrafi <ashikurrafi@gmail.com>
2021-11-04 22:03:25 +0600

    Add project files.

---
+++ Find the largest element in an array using while loop/Find the largest element in an array using while loop.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int size;
+	cout << "Enter size of array : ";
+	cin >> size;
+	int* array;
+	array = new int[size];
+	for (int i = 0; i < size; i++)
+	{
+		cout << "Enter value " << i << " : ";
+		cin >> array[i];
+	}
+	int max = array[0];
+	int i = 0;
+	while (i < size)
+	{
+		if (array[i] > max)
+		{
+			max = array[i];
+		}
+		i++;
+	}
+	cout << "Maximum value : " << max;
+	return 0;
+}
